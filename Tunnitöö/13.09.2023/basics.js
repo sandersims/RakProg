851b9951a40a64148cdb08d959ec6183698b9bc4
Sander <sander02@tlu.ee>
2023-09-13 15:42:23 +0300

    13.09 tunnitöö

--- Tunnitöö/13.09.2023/basics.js
+++ Tunnitöö/13.09.2023/basics.js
@@ -33,5 +33,48 @@ console.log(nameString("Sander"))
 
 const numberArray = [1, 2, 3, 4, 5]
 
-const newArray = numberArray.map((n) => n + 5)
-console.log(newArray)
+// const newArray = numberArray.map((element) => element + 5)
+const newArray = numberArray.map((element, index, array) => {
+    console.log({
+    element: element,
+    newElement: element + 5, 
+    index, 
+    array 
+    })
+
+    return element + 5
+})
+
+
+
+const filteredArray = numberArray.filter(element => element < 4)
+
+console.log({numberArray, filteredArray})
+// console.log(1 < 4)
+
+
+const name = ['Juku', 'Jaanus', 'Mari', 'Anni']
+
+const data = name.map(name => {
+    return {
+        name: name,
+        age: name.length + 20,
+        email: name.toLowerCase() + "@company.com",
+        address: `${name} Street 69`,
+        username: name.split("").reverse().join("")
+    }
+})
+
+
+console.log(data)
+
+const newJuku = {
+    ...data[0],
+    height: 175
+}
+
+const evenNewerJuku = {
+    ...newJuku,
+    age: 99
+}
+console.log({ newJuku, evenNewerJuku }) 
